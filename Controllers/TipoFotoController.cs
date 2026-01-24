3951008c871732e9be473691bf9982f4febefb1d
Guilherme-R-Santos <guilhermersantos99@outlook.com>
2026-01-24 00:05:40 -0300

    Update

--- Controllers/TipoFotoController.cs
+++ Controllers/TipoFotoController.cs
@@ -12,5 +12,27 @@ namespace ImobAPI.Controllers
         {
             _context = context;
         }
+        [HttpPost("Criar")]
+        public IActionResult Create(Entities.TipoFoto tipoFoto)
+        {
+            tipoFoto.DataCadastro = DateTime.Now;
+            tipoFoto.Ativo = true;
+            tipoFoto.Cadastrador = _context.Usuarios.Find(tipoFoto.Cadastrador.Id);
+            if(tipoFoto.Cadastrador == null)
+            {
+                return BadRequest("Cadastrador inválido.");
+            }
+            _context.TiposFoto.Add(tipoFoto);
+            _context.SaveChanges();
+            return Ok(tipoFoto);
+        }
+        [HttpGet("ObterTodos")]
+        public IActionResult GetAll()
+        {
+            var tiposFoto = _context.TiposFoto
+                .Where(tf => tf.Ativo)
+                .ToList();
+            return Ok(tiposFoto);
+        }
     }
 }
